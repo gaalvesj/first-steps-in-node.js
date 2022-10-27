a03d2766cf20eb44565fa55c77c27e0382e76b83
Gabriel <gabrielalves3654@gmail.com>
2022-10-27 20:07:20 -0300

    entendendo o middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express')
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('middleware');
+    // res.send('<h1>first middleware</h1>');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('Another middleware');
+});
+
 const server = http.createServer(app);
     
 server.listen(3000);
