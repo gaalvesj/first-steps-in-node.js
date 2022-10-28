4320331268c72bec6773efb1d7a943acbf28636d
Gabriel <gabrielalves3654@gmail.com>
2022-10-28 15:33:54 -0300

    criando um service com paginas html simples

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const route = express.Router();
 
 route.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
   });
   
   route.post('/product', (req, res, next) => {
