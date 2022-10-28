5562e61dcda9b10ab45e5a99db7cb27c333a1dc1
Gabriel <gabrielalves3654@gmail.com>
2022-10-28 12:10:41 -0300

    entendendo a biblioteca body-parse

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('middleware');
-    // res.send('<h1>first middleware</h1>');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
-app.use((req, res, next) => {
-    console.log('Another middleware');
+
+app.post('/product', (req, res, next) => {
+    //retirando o erro de object null prototype do console.
+    const obj = JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+    // console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
