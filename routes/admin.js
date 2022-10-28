eaa4643859847da0fe0c37971578241dde055c78
Gabriel <gabrielalves3654@gmail.com>
2022-10-28 16:04:44 -0300

    adicionando funcao de path

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
+
 const route = express.Router();
 
 route.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
   });
   
   route.post('/product', (req, res, next) => {
