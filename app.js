b2c422145ef051cd744abf9be2ef921438851262
Gabriel <gabrielalves3654@gmail.com>
2022-10-28 15:38:33 -0300

    adicionando o erro 404 em uma pagina html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+//middleware do page not found
 app.use((req, res) => {
-    res.status(404).send('<h1>Page not found!!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
